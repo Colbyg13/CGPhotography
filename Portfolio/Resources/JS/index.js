4e9fe6a58151fbac8b08221c2a01f134701aaa79
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2021-04-30 01:37:13 -0600

    Prebuild V1.0
    Here I've got the basics added. I will finish the gallery, contact page, and the gear page shortly.

---
+++ Portfolio/Resources/JS/index.js
@@ -0,0 +1,12 @@
+/*
+const aboutMe = document.querySelector("#aboutme");
+var image = document.createElement("img");
+var imageParent = aboutMe;
+image.id = "testImage";
+image.className = "image";
+image.src = "/Resources/CSS/IMAGES/Landscape/provoclimbers.jpg"
+imageParent.appendChild(image);
+
+console.log(aboutMe);
+how to add image programatically
+*/
