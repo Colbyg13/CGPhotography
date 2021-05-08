4975fe7b3bb5973dcaf698d48129bfa9bcf908cc
Colby Gardiner <31553683+Colbyg13@users.noreply.github.com>
2021-05-08 04:49:25 -0600

    Commit 2.0
    Lots of new stuff added. Added more images to gallery, added some responsive design. Still working out the kings for iPad and iPhone. Added contact page and footer. Still need to fix navbar and create a sticky navbar for mobile.

---
+++ Portfolio/Resources/JS/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    
+
+});
+
+/*
+const aboutMe = document.querySelector("#portrait");
+var image = document.createElement("img");
+var imageParent = aboutMe;
+image.id = "testImage";
+image.className = "image";
+image.src = ""
+image.classList.add("")
+imageParent.appendChild(image);
+
+console.log(aboutMe);
+//how to add image programatically
+
+*/
+
